2000500ba590ebb4bc7919193f616217579ff84f
lucaspedrocca <lucaspedrocca@gmail.com>
2023-11-04 16:48:10 -0300

    Orden y creacion de carpeta para p4e

---
+++ p4e/webServices.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+import json
+
+
+data = '''<person>
+    <name>Lucas</name>
+    <phone type="intl">
+        +54 351 152 000 000
+    </phone>
+    <email hide="yes"/>
+</person>'''
+
+tree = ET.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
+
+#imporrt json
+
+data = '''{
+    "name": "Lucas",
+    "phone": {
+        "type": "intl",
+        "number": "+54 351 152 000 000"
+    },
+    "email": {
+        "hide": "yes"
+    }
+}'''
+
+info = json.loads(data)
+print('Name:', info["name"])
+print('Hide:', info["email"]["hide"])
+
+
