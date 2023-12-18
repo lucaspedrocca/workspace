fff512d710962b46bd31f79321a2c203d4056676
lucaspedrocca <lucaspedrocca@gmail.com>
2023-12-18 06:40:00 -0300

    Paso las primeras pruebas de ejemplo.

--- p4e/Proyectos Finales/boilerplate-time-calculator/time_calculator.py
+++ p4e/Proyectos Finales/boilerplate-time-calculator/time_calculator.py
@@ -11,7 +11,7 @@ def add_time(start, duration, day=""):
   durationMinutes = int(durationSplit[0].split(":")[1])
   
   # Definicion de resto de variables
-  dayFormated = day.lower()
+  dayFormated = day.capitalize()
   days = 0
   listDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
@@ -28,60 +28,48 @@ def add_time(start, duration, day=""):
   
   # funcion para convertir a 12 horas
   def convertFormat12(hour):
+    period = "AM" if hour <= 12  else "PM"
     if hour == 0:
       hour = 12
 
     elif hour >= 13:
       hour -= 12
-
-    period = "AM" if hour <= 12  else "PM"
-
     return hour, period
 
 
   
   startHour = convertFormat24(startHour, startAmPm)
 
+  while durationMinutes > 0:
+      # Suma de horas por minuto 
+    startMinutes += durationMinutes
+    while startMinutes >= 60:
+      startHour += 1
+      startMinutes -= 60
+    durationMinutes = 0
+
   while durationHour > 0:
     # Conteo de horas
     startHour += 1
     durationHour -= 1
 
-    # Suma de horas por minuto 
-    if durationMinutes > 0:
-      startMinutes += durationMinutes
-      while startMinutes >= 60:
-        startHour += 1
-        startMinutes -= 60
-      durationMinutes = 0
-
     # Conteo de dias
-    if startHour == 24:
+    if startHour >= 24:
       days += 1
       startHour = startHour - 24
     
-
-  startHour = convertFormat12(startHour)[0]
-  startAmPm = convertFormat12(startHour)[1]
-
-  # if startHour < 10:
-  #   startHour = f"0{startHour}"
+  convertedHour = convertFormat12(startHour)
+  startAmPm = convertedHour[1]
+  startHour = convertedHour[0]
   
   if startMinutes < 10:
     startMinutes = f"0{startMinutes}"
-
-
 
   # Diferentes resupuestas por caso
 
   # Caso 1: No se define day ni pasan dias
 
   new_time = f"Returns: {startHour}:{startMinutes} {startAmPm}"
-
-  #   # Caso 1: No se definio day ni pasaron dias
-  # if day == "" and days == 0:
-  #   new_time = returnHora
-
 
   # Caso 2: Se definio day
   if dayFormated in listDays:
@@ -119,186 +107,3 @@ print(add_time("11:43 AM", "00:20"))
 print(add_time("10:10 PM", "3:30"))
 print(add_time("11:43 PM", "24:20", "tueSday"))
 print(add_time("6:30 PM", "205:12"))
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#   # funcion para convertir a 12 horas
-#   def convertFormat12(a,b):
-#     if b == 'PM':
-#       a = int(a) - 12
-#       if b == 'AM':
-#         b = 'PM'
-#       else:
-#         b
-#     elif a in (0,24):
-#       a = 12
-#     else:
-#       a
-#     return a
-  
-#   # def sumaStr(a,b):
-#   #   c = str(int(a) + int(b))
-#   #   return c
-  
-#   # def restaStr(a,b):
-#   #   c = str(int(a) - int(b))
-#   #   return c
-  
-#   # suma de minutos
-#   totalMinutes = startMinutes + durationMinutes
-  
-#   while totalMinutes >= 60:
-#     durationHour += 1
-#     totalMinutes -= 60
-
-#   # calculo de dias
-
-#   while durationHour >= 24:
-#     days += 1
-#     durationHour = durationHour - 24
-
-#   startHour = convertFormat24(startHour, startAmPm)
-
-#   # totalHour = 0
-#   # if totalHour > 12:
-#   #   totalHour = startHour + durationHour - 12
-#   # else:
-
-#   totalHour = startHour + durationHour
-#   totalHour = convertFormat12(totalHour, startAmPm)
-
-
-#   # formateo de variables
-
-#   if totalMinutes < 10:
-#     totalMinutes = '0' + str(totalMinutes)
-
-#   # Devolucion por caso
-
-#   # Si se declara el dia que empieza
-#   dayFormated = day.capitalize()
-#   if dayFormated in listDays:
-#     indexDay = listDays.index(dayFormated)
-
-#     while (indexDay + days) >= 6:
-#       days -= 7
-#     else:
-#       indexDay += days
-    
-#     dayFormated = listDays[indexDay]
-#     new_time = f"{returns} {totalHour}:{totalMinutes} {startAmPm}, {dayFormated}"
-      
-#   # Cero dias
-#   elif days == 0:
-#     new_time = f"{returns} {totalHour}:{totalMinutes} {startAmPm}"
-
-#   # Un dia
-#   elif days == 1:
-#     new_time = f"{returns} {totalHour}:{totalMinutes} {startAmPm} (next day)"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # if startAmPm == 'AM':
-  #   new_time = returns + sumaStr(startHour, durationHour) + ":" + sumaStr(startMinutes, durationMinutes) + " " + startAmPm
-
-  # else:
-  #   startHour = convertFormat24(startHour)
-
-  #   while durationHour > 24:
-  #     days += 1
-  #     durationHour -= 24
-    
-  #   if days == 1:
-  #     new_time = returns + sumaStr(startHour, durationHour) + ":" + sumaStr(startMinutes, durationMinutes) + " " + startAmPm
