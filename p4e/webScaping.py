c135efd3b027c60611841cbaff21fe9caf70ae15
lucaspedrocca <lucaspedrocca@gmail.com>
2024-01-18 02:14:05 -0300

    Sincronizacion branch principal pre OracleONE

--- p4e/webScaping.py
+++ p4e/webScaping.py
@@ -6,7 +6,7 @@ html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
-tags = soup('a') # a is anchor tag
+tags = soup('h2') # a is anchor tag
 for tag in tags:
-    print(tag.get('href', None))
+    print(tag, None) 
 
