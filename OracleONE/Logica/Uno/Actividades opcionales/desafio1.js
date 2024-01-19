c299463b6af39d268318f1b71fb99eeebc3a2303
lucaspedrocca <lucaspedrocca@gmail.com>
2024-01-19 04:48:17 -0300

    Oracle ONE: Logica 1

---
+++ OracleONE/Logica/Uno/Actividades opcionales/desafio1.js
@@ -0,0 +1,12 @@
+let DiaUsuario = prompt('Indique en que dia de la semana se encuentra:');
+
+if (DiaUsuario == "Sábado"){
+    alert("¡Buen fin de semana!")
+
+} else if(DiaUsuario == "Domingo"){
+    alert("¡Buen fin de semana!")
+
+} else{
+    alert("¡Buena semana!")
+
+}
