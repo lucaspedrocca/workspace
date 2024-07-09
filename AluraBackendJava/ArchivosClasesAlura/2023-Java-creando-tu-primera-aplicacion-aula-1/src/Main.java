00a9e09fdff5bebda9c7885580dd845b2bf8eefb
Lucas Pedrocca <lucaspedrocca@gmail.com>
2024-07-09 15:23:37 -0300

    Primeros clases alura Backend Java

---
+++ AluraBackendJava/ArchivosClasesAlura/2023-Java-creando-tu-primera-aplicacion-aula-1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matrix");
+
+    }
+}
