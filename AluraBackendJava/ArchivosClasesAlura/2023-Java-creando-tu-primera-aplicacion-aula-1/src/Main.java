7055f320c9d636583b92798ea71a7e298c4d2c2f
lucaspedrocca <lucaspedrocca@gmail.com>
2024-07-15 15:20:15 -0300

    Limpieza de folders innecesarios.

--- AluraBackendJava/ArchivosClasesAlura/2023-Java-creando-tu-primera-aplicacion-aula-1/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Bienvenido(a) a Screen Match");
-        System.out.println("Película: Matrix");
-
-    }
-}
