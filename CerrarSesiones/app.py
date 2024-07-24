7fed050091360a6ab4f9cd0850734b04450e2316
lucaspedrocca <lucaspedrocca@gmail.com>
2024-07-24 11:40:06 -0300

    Version 1.0 de app.py

--- CerrarSesiones/app.py
+++ CerrarSesiones/app.py
@@ -1,35 +1,76 @@
-import mysql.connector
+import json
+import os
 import subprocess
 
-# Conectar a la base de datos
-db_config = {
-    'user': 'cerrarSesiones',
-    'password': '10SesionesCerradas.',
-    'host': 'PCA1195.prominente.com.ar',
-    'database': 'servidores_db',
-}
+# Funciones para leer y escribir el archivo JSON
+def load_hostnames():
+    with open('hostnames.json', 'r') as file:
+        data = json.load(file)
+        # Normalizar nombres de empresas a mayúsculas
+        normalized_data = {k.upper(): v for k, v in data.items()}
+        return normalized_data
 
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
+def save_hostnames(hostnames_empresas):
+    try:
+        with open('CerrarSesiones/hostnames.json', 'w') as file:
+            # Normalizar nombres de empresas a mayúsculas
+            normalized_data = {k.upper(): v for k, v in hostnames_empresas.items()}
+            json.dump(normalized_data, file, indent=4)
+        print("Archivo guardado exitosamente.")
+    except Exception as e:
+        print(f"Error al guardar el archivo: {e}")
 
-# Función para obtener los hostnames de los servidores
-def get_hostnames(empresa):
-    query = "SELECT hostname FROM servidores WHERE empresa = %s"
-    cursor.execute(query, (empresa,))
-    return [row[0] for row in cursor.fetchall()]
+def show_servers_for_company(company_name):
+    """Muestra la lista de servidores para una empresa específica."""
+    company_name = company_name.upper()
+    if company_name in hostnames_empresas:
+        print(f"\nLista de servidores para {company_name}:")
+        for server in hostnames_empresas[company_name]:
+            print(f"- {server}")
+    else:
+        print(f"\nNo hay servidores registrados para {company_name}.")
 
-# Opciones generales
+def display_hostnames_json():
+    """Muestra el contenido completo del archivo JSON."""
+    print("\nContenido del archivo hostnames.json:")
+    with open('CerrarSesiones/hostnames.json', 'r') as file:
+        content = json.load(file)
+        # Normalizar nombres de empresas a mayúsculas para la visualización
+        for company, servers in content.items():
+            print(f"\n{company.upper()}:")
+            for server in servers:
+                print(f" - {server}")
+
+def handle_company_query(empresa_consultar):
+    #Maneja la consulta de una empresa específica o muestra el contenido completo del archivo JSON.
+    if empresa_consultar in hostnames_empresas:
+        show_servers_for_company(empresa_consultar)
+    elif empresa_consultar == "ALL":
+        display_hostnames_json()  # Mostrar el contenido del archivo JSON
+    else:
+        print("Ingrese un valor valido.")
+
+# Inicializar hostnames desde el archivo JSON
+hostnames_empresas = load_hostnames()
+
 consulta_amplia = False
-consulta_amplia_text = "Activado." if consulta_amplia else "Desactivado."
-
-empresas = ['BRH', 'CLIBA', 'EMV', 'HAUG', 'MTV', 'BRT']
 opcion_seleccionada = True
 
-while opcion_seleccionada != 8:
-    print("Seleccione una opción: \n")
-    for idx, empresa in enumerate(empresas, 1):
-        print(f"{idx}- {empresa}")
-    print(f"7- Consulta Amplia - {consulta_amplia_text}\n8- Salir")
+while opcion_seleccionada != 9:
+    empresas = list(hostnames_empresas.keys())
+    conteo_empresas = len(empresas)
+    consulta_amplia_text = "Activado." if consulta_amplia else "Desactivado." 
+    consultas_extras = [f'Consulta amplia - {consulta_amplia_text}', 'Editar servidores', 'Salir']
+
+    print("\nSeleccione una opción: \n")
+    for empresa in empresas:
+        posicion = empresas.index(empresa) + 1
+        print(f"{posicion}- {empresa}")
+    
+    for consulta in consultas_extras:
+        posicion_consulta = consultas_extras.index(consulta) + 1
+        print(f"{posicion_consulta + conteo_empresas}- {consulta}")
+        
 
     opcion_seleccionada = input("Ingrese una opción: ")
 
@@ -39,10 +80,11 @@ while opcion_seleccionada != 8:
         print("\nIngrese una opción valida.\n")
         continue
 
-    if opcion_seleccionada in range(1, 7):
+    if opcion_seleccionada in range(1, conteo_empresas + 1):
         empresa_seleccionada = empresas[opcion_seleccionada - 1]
         usuario_ingresado = str(input("Ingrese el nombre del usuario: "))
-        servidores_consulta = get_hostnames(empresa_seleccionada)
+        servidores_consulta = hostnames_empresas[empresa_seleccionada]
+
         print(f"\nRealizando consultas en los servidores de {empresa_seleccionada} para el usuario {usuario_ingresado}.\n")
 
         for servidor in servidores_consulta:
@@ -53,38 +95,85 @@ while opcion_seleccionada != 8:
             sesion_encontrada = resultado.stdout
             list_sesion_encontrada = sesion_encontrada.split()
 
-            id_sesion_encontrada = int(list_sesion_encontrada[2]) if len(list_sesion_encontrada) >= 2 else 99999
+            if len(list_sesion_encontrada) >= 2:
+                id_sesion_encontrada = int(list_sesion_encontrada[2])
+            else:
+                id_sesion_encontrada = 99999
 
             if resultado.returncode != 1:
                 print(f"{servidor}: Hubo un error en la ejecución {resultado.stderr}.\n")
+            
             elif resultado.stdout == "":
-                print(f"{servidor}: El usuario no esta logueado.")
+                print(f"{servidor}: El usuario no está logueado.")
                 if consulta_amplia:
                     resultado_completo = subprocess.run(['powershell', '-Command', consulta_sesion_powershell_completo], capture_output=True, text=True)
                     print(f"{servidor}{resultado_completo.stdout}\n")
+            
             else:
-                print(f"{servidor}:{sesion_encontrada}")
-                cerrar_sesion = int(input("¿Quiere cerrar la sesion encontrada?(1- Cerrar, 2- No cerrar): "))
+                print(f"{servidor}: {sesion_encontrada}")
+                cerrar_sesion = int(input("¿Quiere cerrar la sesión encontrada? (1- Cerrar, 2- No cerrar): "))
+                
                 if cerrar_sesion == 1:
                     while resultado.stdout != "":
                         cierre_sesion_powershell = f"reset session {id_sesion_encontrada} /server:{servidor}"
-                        subprocess.run(['powershell', '-Command', cierre_sesion_powershell], capture_output=True, text=True)
+                        ejecucion_cierre = subprocess.run(['powershell', '-Command', cierre_sesion_powershell], capture_output=True, text=True)
                         resultado_completo = subprocess.run(['powershell', '-Command', consulta_sesion_powershell_completo], capture_output=True, text=True)
                         resultado = subprocess.run(['powershell', '-Command', consulta_sesion_powershell], capture_output=True, text=True)
-                        print(f"{servidor}: Se cerro la sesion de {usuario_ingresado}.")
+
+                        print(f"{servidor}: Se cerró la sesión de {usuario_ingresado}.")
                         if consulta_amplia:
                             print(f"{servidor}{resultado_completo.stdout}\n")
                 else:
                     print("...")
-    elif opcion_seleccionada == 7:
+
+    elif opcion_seleccionada == (conteo_empresas + 1):
         consulta_amplia = not consulta_amplia
         consulta_amplia_text = "Activado" if consulta_amplia else "Desactivado"
-        print(f"Consulta Amplia - {consulta_amplia_text}")
-    elif opcion_seleccionada == 8:
+
+    elif opcion_seleccionada == (conteo_empresas + 2):
+        print("\n1- Añadir Servidor\n2- Eliminar Servidor\n3- Consultar hostnames\n4- Volver")
+        sub_opcion = input("Seleccione una opción: ")
+
+        try:
+            sub_opcion = int(sub_opcion)
+        except:
+            print("\nIngrese una opción valida.\n")
+            continue
+
+        if sub_opcion == 1:
+            empresa_nueva = input("Ingrese el nombre de la empresa: ").upper()
+            handle_company_query(empresa_nueva)
+            hostname_nuevo = input("Ingrese el hostname del servidor: ")
+
+            if empresa_nueva in hostnames_empresas:
+                hostnames_empresas[empresa_nueva].append(hostname_nuevo)
+            else:
+                hostnames_empresas[empresa_nueva] = [hostname_nuevo]
+            save_hostnames(hostnames_empresas)
+            print(f"Servidor {hostname_nuevo} añadido a {empresa_nueva}.")
+            show_servers_for_company(empresa_nueva)  # Mostrar la lista actualizada
+
+        elif sub_opcion == 2:
+            empresa_eliminar = input("Ingrese el nombre de la empresa: ").upper()
+            handle_company_query(empresa_eliminar)
+            hostname_eliminar = input("Ingrese el hostname del servidor: ")
+            if empresa_eliminar in hostnames_empresas and hostname_eliminar in hostnames_empresas[empresa_eliminar]:
+                hostnames_empresas[empresa_eliminar].remove(hostname_eliminar)
+                if not hostnames_empresas[empresa_eliminar]:
+                    del hostnames_empresas[empresa_eliminar]
+                save_hostnames(hostnames_empresas)
+                print(f"Servidor {hostname_eliminar} eliminado de {empresa_eliminar}.")
+                show_servers_for_company(empresa_eliminar)  # Mostrar la lista actualizada
+            else:
+                print(f"Servidor {hostname_eliminar} no encontrado en {empresa_eliminar}.")
+
+        elif sub_opcion == 3:
+            empresa_consultar = input("Ingrese el nombre de la empresa(ALL): ").upper()
+            handle_company_query(empresa_consultar)
+
+    elif opcion_seleccionada == (conteo_empresas + 3):
         break
+
     else:
         print("Ingrese una opción valida.")
         continue
-
-cursor.close()
-conn.close()
