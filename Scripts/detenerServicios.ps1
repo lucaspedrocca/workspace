e1771bf321917918957b41f0bb56efad4771832f
lucaspedrocca <lucaspedrocca@gmail.com>
2024-08-02 10:27:27 -0300

    Reorganizacion de scripts y division en carpetas ""

--- Scripts/detenerServicios.ps1
+++
@@ -1,7 +0,0 @@
-# Obtener todos los servicios que empiezan con "x"
-$serviciosPectra = Get-Service -Name Pectra*
-
-# Detener cada servicio encontrado
-foreach ($servicio in $serviciosPectra) {
-    Stop-Service -Name $servicio.Name -Force
-}
