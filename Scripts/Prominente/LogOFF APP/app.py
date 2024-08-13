18e1bac33aacdda6cf49062dfa51649e09f08377
Pedrocca <lpedrocca@grupoprominente.com>
2024-08-13 13:56:46 -0300

    Implementación de id diferenciando estado de la sesion 140.

--- Scripts/Prominente/LogOFF APP/app.py
+++ Scripts/Prominente/LogOFF APP/app.py
@@ -139,11 +139,19 @@ while opcion_seleccionada != 9:
             list_sesion_encontrada = sesion_encontrada.split()
 
             if len(list_sesion_encontrada) >= 3:
-                try:
-                    id_sesion_encontrada = int(list_sesion_encontrada[2])
-                except ValueError:
-                    print(f"No se pudo convertir el valor '{list_sesion_encontrada[2]}' a un número entero.")
-                    id_sesion_encontrada = None
+                if not "Activo" in list_sesion_encontrada:
+                    try:
+                        id_sesion_encontrada = int(list_sesion_encontrada[1])
+                    except ValueError:
+                        print(f"No se pudo convertir el valor '{list_sesion_encontrada[1]}' a un número entero.")
+                        id_sesion_encontrada = None
+                else:
+                    try:
+                        id_sesion_encontrada = int(list_sesion_encontrada[2])
+                    except ValueError:
+                        print(f"No se pudo convertir el valor '{list_sesion_encontrada[2]}' a un número entero.")
+                        id_sesion_encontrada = None
+
 
             if resultado.returncode != 1 or resultado.stderr != "":
                 print(f"{servidor}: Hubo un error en la ejecución {resultado.stderr}\n")
